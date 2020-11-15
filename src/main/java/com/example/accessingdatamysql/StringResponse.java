59cfe3ba4407b49a286463d657755f5e86c50ae1
KovacsMarton <43884219+grimnast@users.noreply.github.com>
2020-11-15 20:24:45 +0100

    StringResponse class

---
+++ src/main/java/com/example/accessingdatamysql/StringResponse.java
@@ -0,0 +1,12 @@
+package com.example.accessingdatamysql;
+
+public class StringResponse {
+
+    private String response;
+
+    public StringResponse(String s) {
+        this.response = s;
+    }
+
+    // get/set omitted...
+}
