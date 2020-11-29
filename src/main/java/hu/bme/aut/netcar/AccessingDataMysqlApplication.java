b0f98333a48d049b8ffd9cdfd2ede97919b6b344
Adam <61555703+akunsecured@users.noreply.github.com>
2020-11-29 20:52:51 +0100

    rollback

---
+++ src/main/java/hu/bme/aut/netcar/AccessingDataMysqlApplication.java
@@ -0,0 +1,22 @@
+package hu.bme.aut.netcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class AccessingDataMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingDataMysqlApplication.class, args);
+	}
+}
+
+@RestController
+class HelloController{
+	@GetMapping("/")
+	public String hello() {
+		return "Hello Heroku.";
+	}
+}
